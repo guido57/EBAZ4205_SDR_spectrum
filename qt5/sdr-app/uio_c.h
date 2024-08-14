e868dcb7cd273a4c5eb21e024c1f9502cb7a0ea3
Guido <guido@dogsally.com>
2024-08-14 21:27:20 +0200

    Update uio_c.h

--- qt5/sdr-app/uio_c.h
+++ qt5/sdr-app/uio_c.h
@@ -7,21 +7,20 @@
 
 
 #define DEV_AD9851_GFSK     "/dev/uio0"  //0x4000
-#define DEV_AD9851          "/dev/uio1"  //0x412A
-#define DEV_ADC_TEST_SWITCH "/dev/uio2"  //0x4121
-#define DEV_DDS_TEST_GEN    "/dev/uio3"  //0x43C2
-#define DEV_DDS_LO_FT       "/dev/uio4"  //0x4126
-#define DEV_DDS_LO          "/dev/uio5"  //0x43C6
-#define DEV_IF_FT_GAIN      "/dev/uio6"  //0x4128
-#define DEV_DEC_RATE_FT_IQ  "/dev/uio7"  //0x4124
-#define DEV_FT_CAPTURE      "/dev/uio8"  //0x43C1
-#define DEV_IF_GAIN         "/dev/uio9"  //0x4122
-#define DEV_DEC_RATE_IQ     "/dev/uio10" //0x43C3
-#define DEV_HDMI_GPIO       "/dev/uio11" //0x4120
-#define DEV_PS2_MOUSE_RESET "/dev/uio12" //0x4127
-#define DEV_AM_SSB          "/dev/uio13" //0x4125
-#define DEV_DDS_BFO         "/dev/uio14" //0x4129
-#define DEV_RF_CAPTURE      "/dev/uio15" //0x43C0
+#define DEV_ADC_TEST_SWITCH "/dev/uio1"  //0x4121
+#define DEV_DDS_TEST_GEN    "/dev/uio2"  //0x43C2
+#define DEV_DDS_LO_FT       "/dev/uio3"  //0x4126
+#define DEV_DDS_LO          "/dev/uio4"  //0x43C6
+#define DEV_IF_FT_GAIN      "/dev/uio5"  //0x4128
+#define DEV_DEC_RATE_FT_IQ  "/dev/uio6"  //0x4124
+#define DEV_FT_CAPTURE      "/dev/uio7"  //0x43C1
+#define DEV_IF_GAIN         "/dev/uio8"  //0x4122
+#define DEV_DEC_RATE_IQ     "/dev/uio9" //0x43C3
+#define DEV_HDMI_GPIO       "/dev/uio10" //0x4120
+#define DEV_PS2_MOUSE_RESET "/dev/uio11" //0x4127
+#define DEV_AM_SSB          "/dev/uio12" //0x4125
+#define DEV_DDS_BFO         "/dev/uio13" //0x4129
+#define DEV_RF_CAPTURE      "/dev/uio14" //0x43C0
 
 struct UIO
 {
